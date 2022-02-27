4bfb1d863ad7b5e7e1a3f09afc8cf45199ecac63
wilsonfer31 <59471515+wilsonfer31@users.noreply.github.com>
2022-02-27 19:10:12 +0100

    Update relation.js

--- commands/music/relation.js
+++ commands/music/relation.js
@@ -25,9 +25,9 @@ module.exports = class JoinCommand extends Command{
     async run(message){
        
 
-          return message.say("`" + "Relation Coleur-Vehicule \n" + "One-To-one : un Vehicule ne peut avoir qu'une seule Coleur et une Coleur ne peut appartenir qu'un Vehicule \n" 
-          + "Many-to-one exemple : Un Vehicule ne peut avoir qu'une seule Couleur mais cette coleur peut avoir plusieurs Vehicule \n" + "one-to-many : Un Vehiculepeut avoir plusieurs Couleurs et une Couleur ne peut appartenir qu'un Vehicule \n"+
-          "many-to-many : Un Vehiculepeut avoir plusieurs couleurs et les Couleur peuvent avoir plusieurs Vehicules /n"
+          return message.say("`" + "Relation Vehicule-Couleur \n" + "One-To-one : un Vehicule ne peut avoir qu'une seule Couleur et une Couleur ne peut appartenir qu'un Vehicule \n" 
+          + "Many-to-one exemple : Un Vehicule ne peut avoir qu'une seule Couleur mais cette coleur peut avoir plusieurs Vehicule \n" + "one-to-many : Un Vehicule peut avoir plusieurs Couleurs et une Couleur ne peut appartenir qu'un Vehicule \n"+
+          "many-to-many : Un Vehicule peut avoir plusieurs couleurs et les Couleur peuvent avoir plusieurs Vehicules /n"
           +"`");
     }
 }
